ac92097f852ca35a925eac4f3812794b0a556c69
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-09-15 09:42:02 +0530

    Update pm_menu.py

--- DaisyX/modules/pm_menu.py
+++ DaisyX/modules/pm_menu.py
@@ -45,16 +45,16 @@ def help_markup(modules):
 
 
 STICKERS = (
-    "CAACAgUAAxkBAAJOGmBeli95P073FKVkgc4esfKE4UlXAAIOAgACyavAVkbLMIidWYdyHgQ",
-    "CAACAgUAAxkBAAJOG2BeljABwlCfwzHT1gzyiciBri6_AAIsAgACXBPBVgpGQRz-1qmlHgQ",
-    "CAACAgUAAxkBAAJOHGBeljOJ35CQNnkpnVcgRoHuJX6DAAL3AQACN8TBVm1PIART01cWHgQ",
-    "CAACAgUAAxkBAAJOHWBeljXW9QzYQ51gpCjHZHCF5Ui6AAJ7AgAC3zDBVo2xenp7JYhAHgQ",
-    "CAACAgUAAxkBAAJOHmBeljjU0_FT_QpdUUJBqVUC0nfJAAKYAgACJ_jBVvntHY_8WF27HgQ",
-    "CAACAgUAAxkBAAJOH2BeljrV68mPLu8_6n4edT20Q3IQAAJ9AgACq3LBVmLuZuNPlvkfHgQ",
-    "CAACAgUAAxkBAAJOIGBeljttuniUPykRtzkSZj3SRwKJAAI7AgACNm_BVp8TCkE6ZqCoHgQ",
-    "CAACAgUAAxkBAAJOIWBelj-P_2vtVqtkF2OMlVN3M0N4AAK3AQACSm3BVkXF2voraS2tHgQ",
-    "CAACAgUAAxkBAAJOImBelkJxUBm2rL1iPfMZfk-_9DaOAALrAgAC4T3BVniopXQVsZ4KHgQ",
-    "CAACAgUAAxkBAAJOI2BelkMO0AX_wtAc7hUZz1NixuMlAAKEAwACY4TAViVuNLTBmmkgHgQ",
+    "CAACAgUAAxkBAAJAeWFBb8AiXQsa2R88tk8CT1K0ZyjmAAJ0AgACnhjgVOzmBQRriG5rHgQ",
+    "CAACAgUAAxkBAAJAd2FBb7hLoUAAAVOHnkyVl234Yx55PgAC0QIAAv8G4VQ_k3dbBgherx4E",
+    "CAACAgUAAxkBAAJAdWFBb7JBsChFBNxvb3Rw2fRVBoLFAAKHAwACacPpVPtdRtMZDTp-HgQ",
+    "CAACAgUAAxkBAAJAc2FBb5y0m09qGKCOof25VPQdntF2AAKKAwACCDjgVLzQJqmOnzCuHgQ",
+    "CAACAgUAAxkBAAJAcWFBb5Vp82JAQa5Yu9b78XXPsx18AAK4AwACmNLpVNfMBkpJksFsHgQ",
+    "CAACAgUAAxkBAAJAb2FBb48DPvathhvsqYQtqEFokjjrAAJcAgACPrrpVIA9qkFNhZhSHgQ",
+    "CAACAgUAAxkBAAJAe2FBcGzrSvlRAWGeSiK7DqH6vzstAAIFBAACD1vgVCboWTERJV0HHgQ",
+    "CAACAgUAAxkBAAJAfWFBcIgW5rny6knqEm3eBCkJaBVjAAKnBAACRwXpVD6sVki18QlzHgQ",
+    "CAACAgUAAxkBAAJAf2FBcJixTKHugcRQXTNXaumzZBTmAAKFAwACOlDgVH_WcZIhiTJ9HgQ",
+    "CAACAgUAAxkBAAJAg2FBcK4qsmCs4p9tOQbCSwIPd2KNAAK4AwAC8MDhVG9qf4lutlceHgQ",
 )
 
 
@@ -81,19 +81,19 @@ async def get_start_func(message, strings, edit=False):
     buttons.add(
         InlineKeyboardButton(strings["btn_lang"], callback_data="lang_btn"),
         InlineKeyboardButton(
-            strings["btn_source"], url="https://github.com/TeamDaisyX/"
+            strings["btn_source"], url="https://t.me/IET_Owner/724"
         ),
     )
     buttons.add(
-        InlineKeyboardButton(strings["btn_channel"], url="https://t.me/DaisyXUpdates"),
+        InlineKeyboardButton(strings["btn_channel"], url="https://t.me/IET_Updates"),
         InlineKeyboardButton(
-            strings["btn_group"], url="https://t.me/DaisySupport_Official"
+            strings["btn_group"], url="https://t.me/IET_Support"
         ),
     )
     buttons.add(
         InlineKeyboardButton(
             "👸🏼 Add DaisyX to your group",
-            url=f"https://telegram.me/daisyxbot?startgroup=true",
+            url=f"https://telegram.me/NasriyaManager_BoT?startgroup=true",
         )
     )
     # Handle error when user click the button 2 or more times simultaneously
@@ -135,7 +135,7 @@ async def help_cmd(message, strings):
 async def help_cmd_g(message, strings):
     text = strings["btn_group_help"]
     button = InlineKeyboardMarkup().add(
-        InlineKeyboardButton(text=text, url="https://t.me/DaisyXBOT?start")
+        InlineKeyboardButton(text=text, url="https://t.me/NasriyaManager_BoT?start")
     )
     await message.reply(strings["help_header"], reply_markup=button)
 
